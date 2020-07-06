a0220a6b1285fce67349c2ef7aa8f85b7220d993
makmakkerti <makmakkerti007@gmail.com>
2020-07-06 20:20:42 +0300

    Delete node added

--- src/graph.js
+++ src/graph.js
@@ -35,14 +35,28 @@ class Graph {
     this.adjList[node2.value].edges.push(node1);
   }
 
-  // removeNode(node) {};
+  removeNode(node) {
+    delete this.adjList[node.value];
+    const nodes = Object.keys(this.adjList);
+    // eslint-disable-next-line no-restricted-syntax
+    for (const currentNode of nodes) {
+      const { edges } = this.adjList[currentNode];
+      const index = edges.indexOf(currentNode);
+      if (index > -1) {
+        edges.splice(index, 1);
+      }
+    }
+  }
 }
 
 const adjList = new Graph();
 const node1 = { value: 1 };
 const node2 = { value: 2 };
+const node3 = { value: 3 };
 adjList.addNode(node1);
 adjList.addNode(node2);
+adjList.addNode(node3);
 adjList.addEdge(node1, node2);
-console.log(adjList.toString());
-
+console.log('Two nodes: ', adjList);
+adjList.removeNode(node2);
+console.log('Node was removed: ', adjList);
